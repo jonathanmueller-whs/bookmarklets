ea8cb648332f23bcb83dc71c4a7002cfcb39886d
jonathanmueller-whs <jonathan.mueller@whitehatsec.com>
2022-04-13 16:02:33 -0700

    Add compatibility for EU Splunk

--- scan-response-getter/scan-response-getter.js
+++ scan-response-getter/scan-response-getter.js
@@ -7,13 +7,23 @@ var response_pages = [];
 //console.log(table)
 console.log(rows)
 
-for (var i=0; i< rows.length; i++) {
-	var cells = rows[i].cells
-  getResponsePages(cells, response_pages)
-  
+function getResponses(response_pages){
+    for (var i=0; i< rows.length; i++) {
+        var cells = rows[i].cells;
+        var domain = document.domain;
+    
+        if (domain == "splunk.whitehatsec.com") {
+            getUsResponsePages(cells, response_pages);      
+        } else if (domain == "eu-search.prod.de.whs"){
+            getEuResponsePages(cells, response_pages)
+        } else {
+            alert('Unrecognized domain: ' + domain + '\n')
+            return false;
+        } 
+    }
 }
 
-function getResponsePages (cells,response_pages) {
+function getUsResponsePages (cells,response_pages) {
   var rid = cells[1].textContent + "_" + cells[2].textContent + "_" + cells[3].textContent + "_" + cells[3].textContent
   console.log(rid)
   var view_response = "https://console.whitehatsec.com/scan_response.html?rid=" + rid
@@ -21,11 +31,26 @@ function getResponsePages (cells,response_pages) {
   response_pages.push(view_response)
 }
 
-function openTabs(response_pages) {
-  for (var i=0; i < response_pages.length; i++){
-    console.log(response_pages[i]);
-    window.open(response_pages[i]);
+function getEuResponsePages (cells,response_pages) {
+    var rid = cells[1].textContent + "_" + cells[2].textContent + "_" + cells[3].textContent + "_" + cells[3].textContent
+    console.log(rid)
+    var view_response = "https://console.whitehatsec.eu/scan_response.html?rid=" + rid
+    //console.log(view_response);
+    response_pages.push(view_response)
   }
+
+function openTabs(response_pages) {
+    if (response_pages.length > 0){
+        for (var i=0; i < response_pages.length; i++){
+            console.log(response_pages[i]);
+            window.open(response_pages[i]);
+          }
+    } else {
+        alert('No responses found.');
+        return false;
+    }
+  
 }
+getResponses(response_pages)
 
 openTabs(response_pages);
